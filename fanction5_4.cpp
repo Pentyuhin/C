68615c7a4bb4086de508ea572140bea6a6639881
Иван Александрович Пентюхин <pentyuhin-ia@office.local>
2024-04-15 14:56:03 +0300

    added HW 5 task 1, 2, 4, 5

---
+++ fanction5_4.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <iomanip>
+#include "Header.h"
+
+using namespace std;
+
+void shop_day()
+{
+    cout << setw(17) << "ДЕНЬ " << setw(19) << "ВРЕМЯ ЗАКРЫВАЕТСЯ \n\n";
+
+    for (int count = static_cast<int>(Day::Monday); count <= static_cast<int>(Day::Sunday); count++)
+    {
+        cout << setw(19);
+        print_day(static_cast<Day>(count), cout);
+        cout << setw(9);
+        closing_time(static_cast<Day>(count), cout);
+    }
+}
+
+void print_day(Day day, ostream& os)
+{
+    switch (day)
+    {
+    case Day::Monday: os << "Понедельник"; break;
+    case Day::Tuesday: os << "Вторник"; break;
+    case Day::Wednesday: os << "Среда"; break;
+    case Day::Thursday: os << "Четверг"; break;
+    case Day::Friday: os << "Пятница"; break;
+    case Day::Saturday: os << "Суббота"; break;
+    case Day::Sunday: os << "Воскресенье"; break;
+    default: os << "ОШИБКА!"; break;
+    }
+}
+
+void closing_time(Day day_of_the_week, ostream& os)
+{
+    switch (day_of_the_week)
+    {
+    case Day::Sunday: os << "13:00"; break;
+    case Day::Saturday: os << "17:00"; break;
+    case Day::Wednesday: os << "20:00"; break;
+    default: os << "18:00"; break;
+    }
+}
---
+++ main5_5.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <fstream>
+
+
+
+using namespace std;
+
+int main() 
+{
+
+	fstream file(__FILE__); 
+	
+	cout.width(11);
+	cout << "CHAR ";
+	cout.width(15);
+	cout << " NUMBER OF OCCURRENCES\n\n";
+
+	if (file.is_open())
+	{
+		int letterCount[26] = { 0 };
+		char letter;
+
+		while (file.get(letter))
+		{
+
+			if (islower(letter)) 
+			{
+				letterCount[letter - 'a']++;
+			}
+		}
+
+		for (int i = 0; i < 26; ++i)
+		{
+			cout.width(8);
+			cout << char('a' + i);
+			cout.width(10);
+			cout << letterCount[i] << "\n";
+		}
+
+		file.close();
+	}
+	else
+	{
+		cout << "Error opening file";
+	}
+
+	return 0;
+}
