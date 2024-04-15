a612f54ea27811b1c8136856fdebc5c817480b20
Иван Александрович Пентюхин <pentyuhin-ia@office.local>
2024-04-15 16:01:09 +0300

    added hw 6 tast 1

---
+++ main6_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <windows.h>
+#include "IntegerArray.h"
+
+using namespace std;
+
+
+
+int main()
+{
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	input_array(arr_number, MAXLENGTH_ARRAY);
+
+	display_array(arr_number, MAXLENGTH_ARRAY);
+
+	copy_array(arr_number, second_array, MAXLENGTH_ARRAY);
+
+	cout << endl;
+
+	cout << "Стандартное отклонение: " << standard_deviation(arr_number, MAXLENGTH_ARRAY) << endl;
+
+	return 0;
+}
