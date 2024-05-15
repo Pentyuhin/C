3b395b18ad15d83969401402f5e683baaaf9c565
Иван Александрович Пентюхин <pentyuhin-ia@office.local>
2024-05-15 17:20:26 +0300

    added HW 2 2 4

---
+++ queen.h
@@ -0,0 +1,24 @@
+﻿#ifndef QUEEN_H
+#define QUEEN_H
+
+#include <vector>
+
+class Queen {
+public:
+    Queen();
+    void solve();
+    void printSolutions();
+    void saveSolutionsToFile(const char* filename);
+
+private:
+    static const int SIZE = 8;
+    int board[SIZE][SIZE];
+    std::vector<std::vector<int>> solutions;
+    bool isSafe(int row, int col);
+    void clearBoard();
+    void solve(int col);
+    bool isUniqueSolution(const std::vector<int>& solution);
+    void addSolution();
+};
+
+#endif // QUEEN_H
---
+++ testprg.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "queen.h"
+
+int main() {
+
+    setlocale(LC_ALL, "ru");
+    Queen q;
+    q.solve();
+    q.printSolutions();
+    q.saveSolutionsToFile("solutions.txt");
+    return 0;
+}
